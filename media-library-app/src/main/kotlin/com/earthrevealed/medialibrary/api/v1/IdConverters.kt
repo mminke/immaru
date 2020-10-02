b6cff19558daad8accdaf5d8a983dc10b465045f
mminke <m.minke@kataru.nl>
2020-10-02 13:46:46 +0200

    Convert Id classes into a String representation for the rest resources

--- media-library-app/src/main/kotlin/com/earthrevealed/medialibrary/api/v1/IdConverters.kt
+++ media-library-app/src/main/kotlin/com/earthrevealed/medialibrary/api/v1/IdConverters.kt
@@ -3,6 +3,10 @@ package com.earthrevealed.medialibrary.api.v1
 import com.earthrevealed.medialibrary.domain.AssetId
 import com.earthrevealed.medialibrary.domain.CollectionId
 import com.earthrevealed.medialibrary.domain.TagId
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import org.springframework.boot.jackson.JsonComponent
 import org.springframework.core.convert.converter.Converter
 import org.springframework.stereotype.Component
 import java.util.*
@@ -20,4 +24,38 @@ class StringToCollectionIdConverter: Converter<String, CollectionId> {
 @Component
 class StringToTagIdConverter: Converter<String, TagId> {
     override fun convert(uuid: String) = TagId(UUID.fromString(uuid))
+}
+
+
+@JsonComponent
+class TagIdToStringConverter: JsonSerializer<TagId>() {
+    override fun serialize(tagId: TagId?, jsonGenerator: JsonGenerator, serializerProvider: SerializerProvider?) {
+        if(tagId == null) {
+            jsonGenerator.writeNull()
+        } else {
+            jsonGenerator.writeString(tagId.value.toString())
+        }
+    }
+}
+
+@JsonComponent
+class AssetIdToStringConverter: JsonSerializer<AssetId>() {
+    override fun serialize(assetId: AssetId?, jsonGenerator: JsonGenerator, serializerProvider: SerializerProvider?) {
+        if(assetId == null) {
+            jsonGenerator.writeNull()
+        } else {
+            jsonGenerator.writeString(assetId.value.toString())
+        }
+    }
+}
+
+@JsonComponent
+class CollectionIdToStringConverter: JsonSerializer<CollectionId>() {
+    override fun serialize(collectionId: CollectionId?, jsonGenerator: JsonGenerator, serializerProvider: SerializerProvider?) {
+        if(collectionId == null) {
+            jsonGenerator.writeNull()
+        } else {
+            jsonGenerator.writeString(collectionId.value.toString())
+        }
+    }
 }
