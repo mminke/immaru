8f64b7cf83f775b03bcd31615e3b16a7945bdf2a
morten <m.minke@kataru.nl>
2024-10-28 20:51:46 +0100

    fix: upload file using streaming

--- frontend/composeApp/src/commonMain/kotlin/com.earthrevealed.immaru/lightbox/LightboxViewModel.kt
+++ frontend/composeApp/src/commonMain/kotlin/com.earthrevealed.immaru/lightbox/LightboxViewModel.kt
@@ -12,6 +12,7 @@ import com.earthrevealed.immaru.collections.Collection
 import com.earthrevealed.immaru.coroutines.DispatcherProvider
 import com.earthrevealed.immaru.coroutines.awaitFor
 import io.github.vinceglb.filekit.core.PlatformFile
+import io.github.vinceglb.filekit.core.PlatformInputStream
 import io.ktor.utils.io.pool.ByteArrayPool
 import io.ktor.utils.io.pool.useInstance
 import kotlinx.coroutines.launch
@@ -19,6 +20,7 @@ import kotlinx.coroutines.withContext
 import kotlinx.io.Buffer
 import kotlinx.io.RawSource
 import kotlinx.io.buffered
+import kotlin.math.min
 
 class LightboxViewModel(
     private val assetRepository: AssetRepository,
@@ -55,73 +57,71 @@ class LightboxViewModel(
             }
 
             // Transfer the file
-            if (false && file.supportsStreams()) {
-                println("SUPPORTS STREAMS")
-                val platformInputStream = file.getStream()
-                val contentSource = object : RawSource {
-                    override fun close() {
-                        println("Closing source")
-                        platformInputStream.close()
-                    }
+            transferFile(file, newAsset)
 
-                    override fun readAtMostTo(sink: Buffer, byteCount: Long): Long {
-                        require(byteCount <= Int.MAX_VALUE)
-
-                        println("READING AT MOST TO")
-                        val test = awaitFor {
-                            ByteArrayPool.useInstance { buffer ->
-                                var copied = 0
-                                val bufferSize = buffer.size.toLong()
-
-                                with(DispatcherProvider.io()) {
-                                    println("ENTERING WHILE LOOP")
-                                    while (copied < byteCount) {
-                                        println("COPIED $copied OF $byteCount")
-                                        val rc =
-                                            platformInputStream.readInto(buffer, byteCount.toInt())
-
-                                        println("FINISHED READING INTO BUFFER")
-                                        if (rc == -1) {
-                                            if (copied == 0) copied = -1
-                                            break
-                                        }
-                                        if (rc > 0) {
-                                            sink.write(buffer.copyOf(rc))
-                                            copied += rc
-                                        }
-                                    }
-                                    println("COPIED $copied")
-
-                                    copied
-                                }
-                            }
-                        }
-                        return test.toLong()
-
-//                        return awaitFor {
-//                            println("WAITING FOR RESULT")
-//                            val result = test.await().toLong()
-//                            println("WAITING FINISHED: $result")
-//                            result
-//                        }
-                    }
-                }
-
-                println("START SAVING CONTENT")
-                assetRepository.saveContentFor(newAsset, contentSource.buffered())
-
-            } else {
-                val buffer = Buffer().apply {
-                    val content = file.readBytes()
-                    write(content, 0, content.size)
-               }
-                assetRepository.saveContentFor(newAsset, buffer)
-            }
+            refreshAssets()
         }
+    }
 
-        refreshAssets()
+    private suspend fun transferFile(
+        file: PlatformFile,
+        newAsset: FileAsset
+    ) {
+        if (file.supportsStreams()) {
+            println("Supports streaming")
+            val platformInputStream = file.getStream()
+            val contentSource = PlatformInputStreamSource(platformInputStream)
+
+            assetRepository.saveContentFor(newAsset, contentSource.buffered())
+        } else {
+            println("Fallback to reading entire file into memory")
+            val buffer = Buffer().apply {
+                val content = file.readBytes()
+                write(content, 0, content.size)
+            }
+            assetRepository.saveContentFor(newAsset, buffer)
+        }
     }
 }
 
 val FileAsset.contentUrl
     get() = "${Configuration.immaruUrl}/api/collections/${this.collectionId.value}/assets/${this.id.value}/content"
+
+
+class PlatformInputStreamSource(
+    private val platformInputStream: PlatformInputStream
+) : RawSource {
+    override fun close() {
+        platformInputStream.close()
+    }
+
+    override fun readAtMostTo(sink: Buffer, byteCount: Long): Long {
+        require(byteCount <= Int.MAX_VALUE)
+
+        val numberOfBytesCopied = awaitFor {
+            ByteArrayPool.useInstance { buffer ->
+                var copied = 0
+                val bufferSize = buffer.size
+
+                withContext(DispatcherProvider.io()) {
+                    while (copied < byteCount) {
+                        val rc =
+                            platformInputStream.readInto(buffer, min(byteCount.toInt(), bufferSize))
+
+                        if (rc == -1) {
+                            if (copied == 0) copied = -1
+                            break
+                        }
+                        if (rc > 0) {
+                            sink.write(buffer.copyOf(rc))
+                            copied += rc
+                        }
+                    }
+
+                    copied
+                }
+            }
+        }
+        return numberOfBytesCopied.toLong()
+    }
+}
