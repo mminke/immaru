f8d468faa6b1ae622cca840bb8645cbab19fa38e
morten <m.minke@kataru.nl>
2025-12-06 23:31:42 +0100

    fix: Duplicate Content Root detected

--- frontend/composeApp/build.gradle.kts
+++ frontend/composeApp/build.gradle.kts
@@ -117,7 +117,6 @@ android {
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
         applicationId = "com.earthrevealed.immaru"
