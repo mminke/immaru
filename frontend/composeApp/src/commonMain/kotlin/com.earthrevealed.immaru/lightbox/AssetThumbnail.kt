4cf2bac21c03fe2f0730e8082d3ec77c0503dcb5
mminke <m.minke@kataru.nl>
2026-05-22 16:57:40 +0200

    feat(frontend): Add tooltip for asset name and show single line asset name

--- frontend/composeApp/src/commonMain/kotlin/com.earthrevealed.immaru/lightbox/AssetThumbnail.kt
+++ frontend/composeApp/src/commonMain/kotlin/com.earthrevealed.immaru/lightbox/AssetThumbnail.kt
@@ -5,18 +5,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.earthrevealed.immaru.asset.AssetViewModel
@@ -71,9 +77,31 @@ fun AssetThumbnail(
                 .fillMaxWidth()
                 .padding(4.dp)
         ) {
-            Column {
-                Text(asset.name, color = Color.White)
+            FilenameWithHoverTooltip(
+                name = asset.name,
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilenameWithHoverTooltip(name: String) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+            TooltipAnchorPosition.Above
+        ),
+        state = rememberTooltipState(),
+        tooltip = {
+            PlainTooltip {
+                Text(name) // full filename here
             }
         }
+    ) {
+        Text(
+            text = name,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
