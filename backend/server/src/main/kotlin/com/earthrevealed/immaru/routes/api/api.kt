533d558514fb97c800e37390c7922bce9c5f338c
Morten Minke <m.minke@kataru.nl>
2026-06-16 08:39:48 +0200

    feat(core): Create functionality to create assets for orphaned files (#81)

--- backend/server/src/main/kotlin/com/earthrevealed/immaru/routes/api/api.kt
+++ backend/server/src/main/kotlin/com/earthrevealed/immaru/routes/api/api.kt
@@ -2,17 +2,24 @@ package com.earthrevealed.immaru.routes.api
 
 import com.earthrevealed.immaru.assets.AssetRepository
 import com.earthrevealed.immaru.collections.CollectionRepository
-import io.ktor.server.routing.*
-import io.ktor.utils.io.*
+import com.earthrevealed.immaru.maintenance.MaintenanceService
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.RoutingHandler
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+import io.ktor.utils.io.KtorDsl
 import org.koin.ktor.ext.inject
 
 fun Routing.api() {
     val collectionRepository by inject<CollectionRepository>()
     val assetRepository by inject<AssetRepository>()
+    val maintenanceService by inject<MaintenanceService>()
 
     route("api") {
         collectionApi(collectionRepository, assetRepository)
         assetApi(collectionRepository, assetRepository)
+        maintenanceApi(maintenanceService, collectionRepository)
     }
 }
 
