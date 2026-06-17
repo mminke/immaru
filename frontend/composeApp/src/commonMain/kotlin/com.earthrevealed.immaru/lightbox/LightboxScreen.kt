d723301eed5c1aeb0afc41b988bd06b0bb6b4098
mminke <m.minke@kataru.nl>
2026-06-17 22:00:52 +0200

    feat(frontend): Start improving UI

--- frontend/composeApp/src/commonMain/kotlin/com.earthrevealed.immaru/lightbox/LightboxScreen.kt
+++ frontend/composeApp/src/commonMain/kotlin/com.earthrevealed.immaru/lightbox/LightboxScreen.kt
@@ -1,36 +1,45 @@
 package com.earthrevealed.immaru.lightbox
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.CalendarViewMonth
 import androidx.compose.material.icons.filled.Handyman
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.People
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.adaptive.WindowAdaptiveInfo
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.window.core.layout.WindowWidthSizeClass
 import com.earthrevealed.immaru.assets.Asset
 import com.earthrevealed.immaru.collections.CollectionId
 import com.earthrevealed.immaru.common.CenteredProgressIndicator
 import com.earthrevealed.immaru.common.ErrorMessage
-import com.earthrevealed.immaru.lightbox.LightboxDestinations.BROWSE
-import com.earthrevealed.immaru.lightbox.LightboxDestinations.BY_DATE
-import com.earthrevealed.immaru.lightbox.LightboxDestinations.MAINTENANCE
-import com.earthrevealed.immaru.lightbox.LightboxDestinations.PEOPLE
+import com.earthrevealed.immaru.lightbox.LightboxDestination.BROWSE
+import com.earthrevealed.immaru.lightbox.LightboxDestination.BY_DATE
+import com.earthrevealed.immaru.lightbox.LightboxDestination.MAINTENANCE
+import com.earthrevealed.immaru.lightbox.LightboxDestination.PEOPLE
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
 
-enum class LightboxDestinations(
+enum class LightboxDestination(
     val label: String,
     val icon: ImageVector,
     val contentDescription: String
@@ -41,6 +50,7 @@ enum class LightboxDestinations(
     MAINTENANCE("Maintenance", Icons.Filled.Handyman, "Maintenance tools"),
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LightboxScreen(
     collectionId: CollectionId,
@@ -67,34 +77,61 @@ fun LightboxScreen(
 
     val currentDestination = rememberSaveable { mutableStateOf(BROWSE) }
 
-    NavigationSuiteScaffold(
-        layoutType = determineNavigationSuite(currentWindowAdaptiveInfo()),
-        navigationSuiteItems = {
-            LightboxDestinations.entries.forEach {
-                item(
-                    icon = { Icon(it.icon, contentDescription = it.contentDescription) },
-                    label = { Text(it.label) },
-                    selected = it == currentDestination.value,
-                    onClick = { currentDestination.value = it }
-                )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(collection.name) },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        modifier = Modifier
+                            .clickable { onNavigateBack() }
+                            .padding(16.dp)
+                    )
+                }
+            )
+        },
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .consumeWindowInsets(innerPadding)
+                .padding(innerPadding)
+        ) {
+            NavigationSuiteScaffold(
+                modifier = Modifier
+                    .padding(8.dp),
+                layoutType = determineNavigationSuite(currentWindowAdaptiveInfo()),
+                navigationSuiteItems = {
+                    LightboxDestination.entries.forEach {
+                        item(
+                            icon = { Icon(it.icon, contentDescription = it.contentDescription) },
+                            label = { Text(it.label) },
+                            selected = it == currentDestination.value,
+                            onClick = { currentDestination.value = it },
+                            modifier = Modifier
+                                .padding(end = 8.dp)
+                        )
+                    }
+                }
+            ) {
+                when (currentDestination.value) {
+                    BROWSE -> BrowseAllAssetsView(onNavigateBack, onViewAsset, viewModel)
+                    BY_DATE -> BrowseByDateView(collection, onNavigateBack)
+                    MAINTENANCE -> MaintenanceView(onNavigateBack, onViewAsset, viewModel)
+                    PEOPLE -> Column { Text("People") }
+                }
             }
-        }
-    ) {
-        when (currentDestination.value) {
-            BROWSE -> BrowseAllAssetsView(onNavigateBack, onViewAsset, viewModel)
-            BY_DATE -> BrowseByDateView(collection, onNavigateBack)
-            MAINTENANCE -> MaintenanceView(onNavigateBack, onViewAsset, viewModel)
-            PEOPLE -> Column { Text("People") }
         }
     }
 }
 
 private fun determineNavigationSuite(adaptiveInfo: WindowAdaptiveInfo) = with(adaptiveInfo) {
-    if (showPermanantNavigationDrawer()) {
-        NavigationSuiteType.NavigationDrawer
-    } else {
-        NavigationSuiteScaffoldDefaults.calculateFromAdaptiveInfo(adaptiveInfo)
-    }
+//    if (showPermanantNavigationDrawer()) {
+//        NavigationSuiteType.NavigationRail
+//    } else {
+    NavigationSuiteScaffoldDefaults.calculateFromAdaptiveInfo(adaptiveInfo)
+//    }
 }
 
 private fun WindowAdaptiveInfo.showPermanantNavigationDrawer(): Boolean =
